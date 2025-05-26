947b8d8b924a2348e424174cdf36de9d950f3efe
Eremin Semyon <ereminfrims2004@gmail.com>
2025-05-27 01:16:17 +0300

    Добавлен наклон камеры.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-    public Transform player;
-    public Vector3 offset;
+public class CameraFollow : MonoBehaviour {
+    public Transform player;            // Цель слежения
+    public Vector3 offset;              // Смещение камеры относительно player
+    public float tiltAngle = 30f;       // Угол наклона камеры вниз
 
-    private void LateUpdate()
-    {
-        transform.position = player.position + offset;
+    private void LateUpdate() {
+        Vector3 desiredPosition = player.position + offset;
+        
+        desiredPosition.y = player.position.y + offset.y - Mathf.Tan(Mathf.Deg2Rad * tiltAngle) * offset.z;
+        
+        transform.position = desiredPosition;
+
+        transform.LookAt(player);
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,28 +1,24 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerController : MonoBehaviour
-{
-    public float moveSpeed = 5f;
-    private Rigidbody rb;
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed = 5f;    // Скорость передвижения
+    private Rigidbody rb;           
     private Vector3 movement;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
-    void Update()
-    {
+    void Update() {
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         movement = new Vector3(moveX, 0f, moveZ).normalized * moveSpeed;
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         Vector3 newPosition = rb.position + movement * Time.fixedDeltaTime;
         rb.MovePosition(newPosition);
     }
