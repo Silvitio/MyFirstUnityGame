90f719611a24516f2667e1e716e6dd13455c8628
Eremin Semyon <ereminfrims2004@gmail.com>
2025-05-27 00:43:22 +0300

    Реализован скрипт для перемещения объекта игрока по осям X Z.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class PlayerController
+public class PlayerController : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(movement, Space.World);
+    }
 }
