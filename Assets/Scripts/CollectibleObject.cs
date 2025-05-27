0bb4a3d5bfb4032bb421c1138ae60cf06af4ad99
Eremin Semyon <ereminfrims2004@gmail.com>
2025-05-27 09:58:51 +0300

    Удалены копии собираемых объектов, создан его Prefab, добавлен тэг GameManager и добавлен скрипт для собираемых объектов, который автоматически задаёт им GameManager.

--- Assets/Scripts/CollectibleObject.cs
+++ Assets/Scripts/CollectibleObject.cs
@@ -4,6 +4,11 @@ public class CollectibleObject : MonoBehaviour
 {
     public GameManager gameManager;
 
+    private void Awake()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
